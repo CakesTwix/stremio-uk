640ca978c6bfaf6fdd41c072af46bae742d55700
CakesTwix <oleg.kiryazov@gmail.com>
2023-12-11 22:31:07 +0200

    aniage: 🧹 reformat

--- app/parsers/aniage/__init__.py
+++ app/parsers/aniage/__init__.py
@@ -13,6 +13,7 @@ latestUrl = f"{apiUrl}/v2/anime/find"
 imageUrl = "https://image.aniage.net"
 videoCdn = "https://aniage-video-stream.b-cdn.net/"
 
+
 # Catalog
 @app.get("/catalog/series/aniage.json", tags=["Aniage"])
 async def addon_catalog():
@@ -39,6 +40,7 @@ async def addon_catalog():
             }
 
     return JSONResponse(content=metaPreviews)
+
 
 # Pagination
 @app.get("/catalog/series/aniage/skip={skip}.json", tags=["Aniage"])
@@ -67,17 +69,18 @@ async def addon_catalog_skip(skip: int):
 
     return JSONResponse(content=metaPreviews)
 
+
 # Custom Metadata
 @app.get("/meta/series/aniage/{id}.json", tags=["Aniage"])
 async def addon_meta(id: str):
     async with aiohttp.ClientSession() as session:
         async with session.get(f"{mainUrl}/watch?wid={id}") as response:
-            soup = BeautifulSoup (await response.text(), 'html.parser')
+            soup = BeautifulSoup(await response.text(), "html.parser")
             meta_json = json.loads(soup.find("script", type="application/json").text)
 
             meta = {
                 "meta": {
-                    "id": f'aniage/{id}',
+                    "id": f"aniage/{id}",
                     "type": "series",
                     "name": meta_json["props"]["pageProps"]["title"],
                     "poster": f'{imageUrl}/main/{meta_json["props"]["pageProps"]["posterId"]}',
@@ -91,23 +94,19 @@ async def addon_meta(id: str):
 
     return JSONResponse(content=meta)
 
+
 # Series
 @app.get("/stream/series/aniage/{id}.json", tags=["Aniage"])
 async def addon_stream(id: str):
-    streams = {
-        "streams": []
-    }
+    streams = {"streams": []}
 
     async with aiohttp.ClientSession() as session:
         async with session.get(f"{mainUrl}/watch?wid={id}") as response:
-            soup = BeautifulSoup (await response.text(), 'html.parser')
+            soup = BeautifulSoup(await response.text(), "html.parser")
             meta_json = json.loads(soup.find("script", type="application/json").text)
-            
-            
 
             # Getting list voices
             for voice in meta_json["props"]["pageProps"]["teams"]:
-
                 # String build for get voice names
                 stringTeam = f"{apiUrl}/anime/teams/by-ids?"
                 stringTeam += f"ids={voice['teamId']}&"
@@ -115,34 +114,48 @@ async def addon_stream(id: str):
                     voiceNamesJson = await voiceNames.json()
 
                 # Do list of episodes
-                async with session.get(f"{apiUrl}/anime/episodes?animeId={id}&page=1&pageSize=30&sortOrder=ASC&teamId={voice['teamId']}&volume=1") as episodes:
+                async with session.get(
+                    f"{apiUrl}/anime/episodes?animeId={id}&page=1&pageSize=30&sortOrder=ASC&teamId={voice['teamId']}&volume=1"
+                ) as episodes:
                     index = 0
                     for episode in await episodes.json():
-                        episodeName = f"Серія {episode['episodeNum']}" if episode["title"] in [". ", "."] or episode["title"] == episode["episodeNum"] else f"Серія {episode['episodeNum']} - {episode['title']}"
-                        
+                        episodeName = (
+                            f"Серія {episode['episodeNum']}"
+                            if episode["title"] in [". ", "."]
+                            or episode["title"] == episode["episodeNum"]
+                            else f"Серія {episode['episodeNum']} - {episode['title']}"
+                        )
+
                         url = None
                         if episode["playPath"]:
                             async with session.get(episode["playPath"]) as video:
-                                video_soup = BeautifulSoup (await video.text(), 'html.parser')
+                                video_soup = BeautifulSoup(
+                                    await video.text(), "html.parser"
+                                )
                                 url = video_soup.find("source")["src"]
-                            
+
                         elif episode["s3VideoSource"]:
-                            url = f"{videoCdn}{episode['s3VideoSource']['playlistPath']}"
+                            url = (
+                                f"{videoCdn}{episode['s3VideoSource']['playlistPath']}"
+                            )
 
                         elif episode["videoSource"]:
-                            async with session.get(episode["videoSource"]["playPath"]) as video:
-                                video_soup = BeautifulSoup (await video.text(), 'html.parser')
+                            async with session.get(
+                                episode["videoSource"]["playPath"]
+                            ) as video:
+                                video_soup = BeautifulSoup(
+                                    await video.text(), "html.parser"
+                                )
                                 url = video_soup.find("source")["src"]
-                        
+
                         streams["streams"].append(
                             {
                                 "name": episodeName,
                                 "url": url,
-                                "description": voiceNamesJson[index]["name"]
+                                "description": voiceNamesJson[index]["name"],
                             }
-                        )       
-                
-                index += 1       
-                    
+                        )
+
+                index += 1
 
     return JSONResponse(content=streams)
